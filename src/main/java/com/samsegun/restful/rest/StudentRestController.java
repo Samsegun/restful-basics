2657dba451ae4c240850959d799718b9c5926f05
Samsegun <oyebadesamsegun@gmail.com>
2024-06-12 14:56:43 +0100

    global exception handler

--- src/main/java/com/samsegun/restful/rest/StudentRestController.java
+++ src/main/java/com/samsegun/restful/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package com.samsegun.restful.rest;
 
 import com.samsegun.restful.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,24 +40,10 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student student(@PathVariable int studentId) {
 
-        if((studentId >= students.size()) || studentId < 0) {
+        if ((studentId >= students.size()) || studentId < 0) {
             throw new StudentNotFoundException("Student id not found - " + studentId);
         }
 
         return students.get(studentId);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(error);
     }
 }
